a746ecc33798e0288d705159f0084f5ff58a15c7
Solary-3 <atrnatdacc@gmail.com>
2025-06-27 19:00:38 +0800

    Fix

--- Kdv3Audiopacks.lua
+++ Kdv3Audiopacks.lua
@@ -306,6 +306,11 @@ if not isfile("Dances/APEX.mp3") then
 	game:HttpGet("https://github.com/Solary-3/Scripts/blob/Audios-1/APEX.mp3?raw=true"))
 	warn("APEX Downloaded!")
 end
+if not isfile("Dances/Pixelation.mp3") then
+	writefile("Dances/ABYSSAL.mp3", 
+	game:HttpGet("https://github.com/Solary-3/Scripts/blob/Audios-1/Pixelaion.mp3?raw=true"))
+	warn("Pixelation Downloaded!")
+end
 
 
 game:GetService("StarterGui"):SetCore("SendNotification", { 
