927aef92f30450e4c27dd4d4bb6b148c848b3fbe
Solary-3 <atrnatdacc@gmail.com>
2025-07-11 21:24:58 +0800

    Add files via upload

--- 09c4b345-079a-416e-bcb3-480dc614bedf.lua
+++ 09c4b345-079a-416e-bcb3-480dc614bedf.lua
@@ -2334,7 +2334,7 @@ sound69.SoundId = customasset("Dances/Billy.mp3")
        timeposcur = sound69.TimePosition 
 sound69:Play() 
 coolparticles.Parent = char.Torso
-        playanim(108058940444935,1.25)     
+        playanim(108058940444935,1.65)     
     else
 task.wait(.1)
 
