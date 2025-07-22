c3b3a9f94842b4632a8b603c3754af4d2219d00a
Solary-3 <atrnatdacc@gmail.com>
2025-07-22 18:49:43 +0800

    Update Before.lua

--- Before.lua
+++ Before.lua
@@ -200,6 +200,39 @@ G2L["TOS FRAME_2"]:Destroy()
 notif("Now Loading Krystal Tool Dance V3")
 wait(1.150)
 
+if _G.Loaded3 then return warn("Sup Bro! The Script Is Loaded!") end
+_G.Loaded3 = true
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/Solary-3/Scripts/refs/heads/main/Reanimate.lua"))()
+
+-- SETTINGS --
+local settings = _G
+
+settings["Use default animations"] = true
+settings["Fake character transparency level"] = 1
+settings["Disable character scripts"] = true
+settings["Fake character should collide"] = true
+settings["Parent real character to fake character"] = false
+settings["Respawn character"] = true
+settings["Instant respawn"] = false
+settings["Hide HumanoidRootPart"] = false
+settings["PermaDeath fake character"] = true
+settings["R15 Reanimate"] = false
+settings["Names to exclude from transparency"] = {
+    --[[ example:
+    ["HumanoidRootPart"] = true,
+    ["Left Arm"] = true
+    ]]
+}
+(function() if getgenv then return getgenv() else return _G end end)().fling = nil
+local finished = false
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/somethingsimade/CurrentAngleV2/refs/heads/main/v2"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Solary-3/Scripts/refs/heads/main/KDV3%20Tool%20Version.lua"))()
+end)
+
+repeat task.wait() until finished
+		
 end)
 
 
