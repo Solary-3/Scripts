0f6145da8af47a1b795a071d65cb05d0e76038fe
Solary-3 <atrnatdacc@gmail.com>
2025-08-27 20:50:12 +0800

    Add files via upload

--- Encription Glitcher.lua
+++ Encription Glitcher.lua
@@ -935,13 +935,6 @@ game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("net")
 
 
 
-for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
-if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
-game:GetService("RunService").Heartbeat:connect(function()
-v.Velocity = Vector3.new(30,0,0)
-end)
-end
-end
 
 
 
