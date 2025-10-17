3ef0388c489dfca667fa2f6271aabcbe87913151
Solary-3 <atrnatdacc@gmail.com>
2025-10-17 17:11:32 +0800

    Nas

--- Kdv3Compile.lua
+++ Kdv3Compile.lua
@@ -2,6 +2,9 @@
 if isfolder and not isfolder("KDV3") then 
 	makefolder("KDV3")
 end
+
+
+
 coroutine.resume(coroutine.create(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Solary-3/Scripts/refs/heads/main/Tag"))()
 end))
