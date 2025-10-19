0f19b9c5b005e1100b8df11037e8cbd2e4ed0d83
Solary-3 <atrnatdacc@gmail.com>
2025-10-19 15:45:52 +0800

    Final

--- FinalNeptunian.lua
+++ FinalNeptunian.lua
@@ -7817,8 +7817,8 @@ LH.C0=clerp(LH.C0,cf(-0.500, -.15-.15*math.cos(sine/20), -.15-.15*math.cos(sine/
 RootJoint.C0=clerp(RootJoint.C0,RootCF*cf(0.000, 0.000, -1.000+.25*math.cos(sine/20)) * angles(math.rad(30.500), math.rad(0.000), math.rad(0.000)),.3)
 Torso.Neck.C0=clerp(Torso.Neck.C0,necko*angles(math.rad(-30.000+math.random(-2,2)), math.rad(0.000+math.random(-2,2)), math.rad(-0.000+math.random(2,2))),.3)
 RW.C0=clerp(RW.C0,cf(1.55, 0.500+.15*math.cos(sine/20), 0.550) * angles(math.rad(-0.000), math.rad(-30.000), math.rad(90.000)),.3)
-LW.C0=clerp(LW.C0,cf(-1.500, -0.150-.15*math.cos(sine/20), -0.750) * angles(math.rad(40.000), math.rad(0.000), math.rad(-10.000)),.3)
-weaponweld.C1=clerp(weaponweld.C1,cf(0.5 + kan.PlaybackLoudness/350,0,-1.5)*angles(math.rad(0),math.rad(0),math.rad(190 *sine*.5+kan.PlaybackLoudness/550)),.3)
+LW.C0=clerp(LW.C0,cf(-1.500, -0.150-.25*math.cos(sine/20), -0.750) * angles(math.rad(40.000), math.rad(0.000), math.rad(-10.000)),.3)
+weaponweld.C1=clerp(weaponweld.C1,cf(0.5 + kan.PlaybackLoudness/350,0,-1.5)*angles(math.rad(0),math.rad(0),math.rad(190 *sine+kan.PlaybackLoudness/550)),.3)
 end
 end
 
@@ -7900,7 +7900,7 @@ RootJoint.C0=clerp(RootJoint.C0,RootCF*cf(0,-0.3,-0.05 + 0.15 * math.cos(sine / 
 Torso.Neck.C0=clerp(Torso.Neck.C0,necko*angles(math.rad(-6 - 15 * math.cos(sine / 0.5)),math.rad(6 - 15 * math.cos(sine / 0.5)),math.rad(10 - 20 * math.cos(sine / 0.5))),.3)
 RW.C0=clerp(RW.C0,cf(1.5,0.5,0 + 0.25 * math.cos(sine / 4.6))*angles(math.rad(-40),math.rad(0),math.rad(25 - 2 * math.cos(sine / 34))),.3)
 LW.C0=clerp(LW.C0,cf(-1.5,0.5,0 - 0.5 * math.cos(sine / 6))*angles(math.rad(0 + 70 * math.cos(sine / 6)),math.rad(0),math.rad(-5 + 20 * math.cos(sine / 3))),.3)
-weaponweld.C1=clerp(weaponweld.C1,cf(0,1,0)*angles(math.rad(-10),math.rad(180),math.rad(0)),.3)
+weaponweld.C1=clerp(weaponweld.C1,cf(0,1,0)*angles(math.rad(-7.5),math.rad(180),math.rad(0)),.3)
 end
 end
 
