bd4e56c1e688193f4ef086d4db62f062b843dedc
Solary-3 <atrnatdacc@gmail.com>
2025-10-22 18:09:53 +0800

    fih

--- Kdv3Compile.lua
+++ Kdv3Compile.lua
@@ -61,9 +61,7 @@ if isfolder and not isfolder("KDV3") then
 	makefolder("KDV3")
 end
 coroutine.resume(coroutine.create(function()
-while wait(.005) do
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Solary-3/Scripts/refs/heads/main/Tag"))()
-end
 end))
 if not isfolder("KRYSTALDANCE") then
         makefolder("KRYSTALDANCE")
