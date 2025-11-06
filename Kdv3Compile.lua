6281131d261b2b5f9925d947d828f86d03287e67
Solary-3 <atrnatdacc@gmail.com>
2025-11-07 07:57:31 +0800

    update Kdv3Compile.lua

--- Kdv3Compile.lua
+++ Kdv3Compile.lua
@@ -1106,10 +1106,7 @@ Text3.LineJoinMode = Enum.LineJoinMode.Miter
 
 
 -- Krystal Dance V3, Made by Hemi (es muy janky)
-local lol = math.random(1,30)
-if lol == 2 then 
-	lol = true 
-end
+local lol = math.random(1,2)
 local idleanim = is:LoadLocalAsset("rbxassetid://74204337812128") -- uuid. 136078657506707
 local walkanim = is:LoadLocalAsset("rbxassetid://73688515498572") -- uuid. 130213485744288
 local randompart = Instance.new("Part",game:GetService("RunService"))
@@ -1574,7 +1571,11 @@ end
 if Occasions=="Halloween" then
 Playsound.SoundId = DanceAsset("Halloween.mp3")
 else
+if lol==1 then 
 Playsound.SoundId = DanceAsset("Sphere.mp3")
+else 
+Playsound.SoundId = DanceAsset("TANOC.mp3")
+end
 end 
 Playsound.TimePosition =0
 Playsound.Volume = .75
