b3a678b6ae1ff1e7f727498d74c0ee72940bf85d
Solary-3 <atrnatdacc@gmail.com>
2025-12-10 16:40:40 +0800

    update WhatIsThisScript.lua

--- WhatIsThisScript.lua
+++ WhatIsThisScript.lua
@@ -10,6 +10,8 @@ end
 notify("Check Console.")
 print("Hey there! Its me Theo, now your probably wondering, why did my script stopped entirely? the reason is that im not on the mood into doing scripting and maintaining my script and im getting tired, im not feeling well and im not the same guy youve used to know. I wanna be a different person, even though i still exploit, i had enough fromm those bans that i went through, my account has been put at risk and one of my account got terminated just from exploiting, now  i only have account left that has robux and a good progress waiting for its return. now your probably upset or sad that i stopped the script youve used the most and thats why i have to put it into an end. Its fun while it lasted, its been a wonderful journey weve had went through and hope that someday you and I wont forget the memories weve made throughout this wonderful journey , thank you spending your time with me and using my acripts. \n\n\n - Love from Theo")
 
+print("\n\n\n-- If you ever see a person named White Hat, for the love of god, hes a freak and used a doll script, has a history of beind6a pdf and some of his members call him like that, he acts so freaky near my friend dummy guy before he even renamed himself like that and yet still acts the same way ane never changed his behavio, i also know his age(hes 19) and the friend that hes doi")
+
 
 
 
