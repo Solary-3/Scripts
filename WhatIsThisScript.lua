4fc01a1e03f6fc7a4371440143e04e05cc002a43
Solary-3 <atrnatdacc@gmail.com>
2025-12-21 16:28:19 +0800

    update WhatIsThisScript.lua

--- WhatIsThisScript.lua
+++ WhatIsThisScript.lua
@@ -10,7 +10,6 @@ end
 
 notify("Check Console.")
 print("\n\n\n--Hey there! Its me Theo, now your probably wondering, why did my script stopped entirely? the reason is that im not on the mood into doing scripting and maintaining my script and im getting tired, im not feeling well and im not the same guy youve used to know. I wanna be a different person, even though i still exploit, i had enough fromm those bans that i went through, my account has been put at risk and one of my account got terminated just from exploiting, now  i only have account left that has robux and a good progress waiting for its return. now your probably upset or sad that i stopped the script youve used the most and thats why i have to put it into an end. Its fun while it lasted, its been a wonderful journey weve had went through and hope that someday you and I wont forget the memories weve made throughout this wonderful journey , thank you spending your time with me and using my acripts\n\n Pro Tip: There are no pro tips, you gotta mod on your own :cat_laughing:. \n\n\n - Love from Theo")
-print("\n\n\n -- you can scroll up to read more...")
 print("\n\n-- Last message from me, all of my scripts will never go back, never will, so yeah, its was fun while it lasted.")
 
 
