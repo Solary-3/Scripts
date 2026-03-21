5e11d129c0dc3a66e4123b99108a1e81d69eb324
Solary-3 <atrnatdacc@gmail.com>
2026-03-21 12:00:34 +0800

    update ToolDance.lua

--- ToolDance.lua
+++ ToolDance.lua
@@ -15,7 +15,11 @@ local Players = game:GetService("Players")
 local Players = game:GetService("Players")
 
 local TAG_NAME = "ScriptOwnerTag"
-local TARGET_USERNAMES = {"TH33NERGIZER"}
+local TARGET_USERNAMES = {
+  "TH33NERGIZER",
+  "Hellosundeeundee",
+  
+}
 local CHECK_INTERVAL = 2
 
 local TargetLookup = {}
@@ -24,9 +28,40 @@ for _, name in pairs(TARGET_USERNAMES) do
 end
 
 local function createTag(player)
-	if player.Character and player.Character:FindFirstChild("Head") then
+if player.Character and player.Character:FindFirstChild("Head") then
 if player.Character.Head:FindFirstChild(TAG_NAME) then return end
-
+if player.Name=="Hellosundeeundee" then
+local billboard = Instance.new("BillboardGui")
+billboard.Name = TAG_NAME
+billboard.Size = UDim2.new(3, 0, 2, 0)
+billboard.StudsOffset = Vector3.new(-0.6, 0, 0)
+billboard.Adornee = player.Character.Head
+billboard.AlwaysOnTop = true
+billboard.Parent = player.Character.Head
+local billboard1 = Instance.new("BillboardGui")
+billboard1.Name = TAG_NAME
+billboard1.Size = UDim2.new(0, 100, 0, 40)
+billboard1.StudsOffset = Vector3.new(0, 2.5, 0)
+billboard1.Adornee = player.Character.Head
+billboard1.AlwaysOnTop = true
+billboard1.Parent = player.Character.Head
+local label = Instance.new("TextLabel")
+label.Size = UDim2.new(1, 0, 1, 0)
+label.BackgroundTransparency = 1
+label.Text = "Theo's Friend :D\n Developer"
+label.TextColor3 = Color3.new(1, 1, 0)
+label.TextStrokeTransparency = 0
+label.TextScaled = true
+label.Font = Enum.Font.Sarpanch
+       -- label.Position = UDim2.new(0,0.6,0,-100)
+label.Parent = billboard1
+local imageLabel = Instance.new("ImageLabel")
+local imageLabel = Instance.new("ImageLabel")
+imageLabel.Size = UDim2.new(1, 0, 1, 0)
+imageLabel.BackgroundTransparency = 1
+imageLabel.Image = imageId
+imageLabel.Parent = billboard
+elseif player.Name=="TH33NERGIZER" then 
 local billboard = Instance.new("BillboardGui")
 billboard.Name = TAG_NAME
 billboard.Size = UDim2.new(3, 0, 2, 0)
@@ -52,12 +87,14 @@ label.Font = Enum.Font.Sarpanch
        -- label.Position = UDim2.new(0,0.6,0,-100)
 label.Parent = billboard1
 local imageLabel = Instance.new("ImageLabel")
+local imageLabel = Instance.new("ImageLabel")
 imageLabel.Size = UDim2.new(1, 0, 1, 0)
 imageLabel.BackgroundTransparency = 1
 imageLabel.Image = imageId
 imageLabel.Parent = billboard
 
-	end
+end
+end
 end
 
 task.spawn(function()
