6474d028da988b84be5c67a7bc8814e993d6c5c0
Solary-3 <atrnatdacc@gmail.com>
2026-04-10 13:47:03 +0800

    update ToolDance.lua

--- ToolDance.lua
+++ ToolDance.lua
@@ -23,6 +23,11 @@ setclipboard("https://discord.gg/gR6Pk5pMK")
 		Text = "Join the discord to report bugs or uh for more support!";
 		Duration = 10;
 	})
+	game.StarterGui:SetCore("SendNotification",{
+		Title = "From Theo";
+		Text = "Giveaway at 100 members!";
+		Duration = 10;
+	})
 
 
 
