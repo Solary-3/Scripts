ed92aa13386bc642f5a8cdedafff8026a6186cf4
Solary-3 <atrnatdacc@gmail.com>
2026-04-25 11:41:55 +0800

    update DanceModule1.lua

--- DanceModule1.lua
+++ DanceModule1.lua
@@ -1027,5 +1027,43 @@ AddModule(function()
 	return m
 end)
 
+table.insert(modules, function()
+    local m = {}
 
+    m.ModuleType  = "DANCE"
+    m.Name        = "HypnoDance"
+    m.Description = "Get Hypnotized"
+    m.Assets = {"HypnoDance.anim@https://github.com/Solary-3/Scripts/raw/refs/heads/main/HypnoDance.anim", "HypnoDance.mp3@https://github.com/Solary-3/Scripts/raw/refs/heads/Audios-1/HypnoDance.mp3?raw=true"}
+
+    m.Config = function(parent)
+        Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+    end
+
+    m.SaveConfig = function() return {} end
+    m.LoadConfig  = function(save) end
+
+    local animator = nil
+    local start    = 0
+
+    m.Init = function(figure)
+        SetOverrideDanceMusic(AssetGetContentId("HypnoDance.mp3"), "HypnoDance", 0.8, NumberRange.new(0, 45.5))
+
+        start           = os.clock()
+        animator        = AnimLib.Animator.new()
+        animator.rig    = figure
+        animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("HypnoDance.anim"))
+        animator.looped = true
+        animator.speed  = 1
+    end
+
+    m.Update = function(dt, figure)
+        animator:Step(os.clock() - start)
+    end
+
+    m.Destroy = function(figure)
+        animator = nil
+    end
+
+    return m
+end)
 return modules
