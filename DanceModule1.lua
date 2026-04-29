05a990191016dd245fb47e7a403e66f1f499ba83
Solary-3 <atrnatdacc@gmail.com>
2026-04-29 15:13:10 +0800

    update DanceModule1.lua

--- DanceModule1.lua
+++ DanceModule1.lua
@@ -2424,6 +2424,49 @@ AddModule(function()
     local m = {}
 
     m.ModuleType  = "DANCE"
+    m.Name        = "Rabbit Pole"
+    m.Description = " "
+    m.Assets = {
+      "Rabbit Pole.lua@https://github.com/Solary-3/Scripts/raw/refs/heads/main/Rabbit Pole.lua",
+      "Rabbit Pole.mp3@https://github.com/Solary-3/Scripts/raw/refs/heads/Audios-1/Rabbit Pole.mp3?raw=true"
+    }
+
+    m.Config = function(parent)
+        Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+    end
+
+    m.SaveConfig = function() return {} end
+    m.LoadConfig  = function(save) end
+
+    local animator = nil
+    local start = os.clock()
+
+    m.Init = function(figure)
+        SetOverrideDanceMusic(AssetGetContentId("Rabbit Pole.mp3"))
+
+        start           = os.clock()
+        animator        = AnimLib.Animator.new()
+        animator.rig    = figure
+        animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("Rabbit Pole.lua"))
+        animator.looped = true
+        animator.speed  = 1
+    end
+
+    m.Update = function(dt, figure)
+        animator:Step(os.clock() - start)
+    end
+
+    m.Destroy = function(figure)
+        animator = nil
+    end
+
+    return m
+end)
+
+AddModule(function()
+    local m = {}
+
+    m.ModuleType  = "DANCE"
     m.Name        = "Meme Funk"
     m.Description = " "
     m.Assets = {
