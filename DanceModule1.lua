4c12998393d167e26e67381fa0a5159cc4c609e4
Solary-3 <atrnatdacc@gmail.com>
2026-04-29 23:28:32 +0800

    update DanceModule1.lua

--- DanceModule1.lua
+++ DanceModule1.lua
@@ -2721,4 +2721,47 @@ AddModule(function()
     return m
 end)
 
+AddModule(function()
+    local m = {}
+
+    m.ModuleType  = "DANCE"
+    m.Name        = "Mimbomboclat"
+    m.Description = " "
+    m.Assets = {
+      "Mibombo.lua@https://github.com/Solary-3/Scripts/raw/refs/heads/main/Mibombo.lua",
+      "Mibombo.mp3@https://github.com/Solary-3/Scripts/raw/refs/heads/Audios-1/Mibombo.mp3?raw=true"
+    }
+
+    m.Config = function(parent)
+        Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+    end
+
+    m.SaveConfig = function() return {} end
+    m.LoadConfig  = function(save) end
+
+    local animator = nil
+    local start = os.clock()
+
+    m.Init = function(figure)
+        SetOverrideDanceMusic(AssetGetContentId("Mibombo.mp3"))
+
+        start           = os.clock()
+        animator        = AnimLib.Animator.new()
+        animator.rig    = figure
+        animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("Mibombo.lua"))
+        animator.looped = true
+        animator.speed  = 1
+    end
+
+    m.Update = function(dt, figure)
+        animator:Step(GetOverrideDanceMusicTime())
+    end
+
+    m.Destroy = function(figure)
+        animator = nil
+    end
+
+    return m
+end)
+
 return modules
