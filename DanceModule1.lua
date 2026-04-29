115573da4a466f81610cc82a84869b332880cf5a
Solary-3 <atrnatdacc@gmail.com>
2026-04-29 10:57:45 +0800

    update DanceModule1.lua

--- DanceModule1.lua
+++ DanceModule1.lua
@@ -1607,6 +1607,49 @@ AddModule(function()
 end)
 
 AddModule(function()
+    local m = {}
+
+    m.ModuleType  = "DANCE"
+    m.Name        = "Eggmanned"
+    m.Description = "Get Eggmanned"
+    m.Assets = {
+      "Eggmanned.lua@https://github.com/Solary-3/Scripts/raw/refs/heads/main/Eggmanned",
+      "Eggmanned.mp3@https://github.com/Solary-3/Scripts/raw/refs/heads/Audios-1/Eggmanned.mp3?raw=true"
+    }
+
+    m.Config = function(parent)
+        Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+    end
+
+    m.SaveConfig = function() return {} end
+    m.LoadConfig  = function(save) end
+
+    local animator = nil
+    local start    = 0
+
+    m.Init = function(figure)
+        SetOverrideDanceMusic(AssetGetContentId("Eggmanned.mp3"))
+
+        start           = os.clock()
+        animator        = AnimLib.Animator.new()
+        animator.rig    = figure
+        animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("Eggmanned.lua"))
+        animator.looped = true
+        animator.speed  = 1
+    end
+
+    m.Update = function(dt, figure)
+        animator:Step(os.clock() - start)
+    end
+
+    m.Destroy = function(figure)
+        animator = nil
+    end
+
+    return m
+end)
+
+AddModule(function()
 	local m = {}
 	m.ModuleType = "DANCE"
 	m.Name = "INTERNET YAMERO"
