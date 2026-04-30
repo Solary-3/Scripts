4e37d9c14983454a11a7c575baf0e07dc8a67725
Solary-3 <atrnatdacc@gmail.com>
2026-04-30 16:06:54 +0800

    update DanceModule1.lua

--- DanceModule1.lua
+++ DanceModule1.lua
@@ -2764,4 +2764,90 @@ AddModule(function()
     return m
 end)
 
+AddModule(function()
+    local m = {}
+
+    m.ModuleType  = "DANCE"
+    m.Name        = "Yuji Edit"
+    m.Description = "Ts so tuff"
+    m.Assets = {
+      "Yuji Edit.lua@https://github.com/Solary-3/Scripts/raw/refs/heads/main/Yuji Edit.lua",
+      "Yuji Edit.mp3@https://github.com/Solary-3/Scripts/raw/refs/heads/Audios-1/Yuji Edit.mp3?raw=true"
+    }
+
+    m.Config = function(parent)
+        Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+    end
+
+    m.SaveConfig = function() return {} end
+    m.LoadConfig  = function(save) end
+
+    local animator = nil
+    local start = os.clock()
+
+    m.Init = function(figure)
+        SetOverrideDanceMusic(AssetGetContentId("Yuji Edit.mp3"))
+
+        start           = os.clock()
+        animator        = AnimLib.Animator.new()
+        animator.rig    = figure
+        animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("Yuji Edit.lua"))
+        animator.looped = true
+        animator.speed  = 1
+    end
+
+    m.Update = function(dt, figure)
+        animator:Step(GetOverrideDanceMusicTime())
+    end
+
+    m.Destroy = function(figure)
+        animator = nil
+    end
+
+    return m
+end)
+
+AddModule(function()
+	local m = {}
+
+	m.ModuleType  = "DANCE"
+	m.Name        = "Competent"
+	m.Description = "ts so glee :D:D:D:D:D:D\n-- Valta's Module"
+	m.Assets      = {
+	  "Competent.anim@https://github.com/valttasar/Valttas-Port-Content/raw/refs/heads/main/Competent/Competent.anim",
+	  "Competent.mp3@https://github.com/valttasar/Valttas-Port-Content/raw/refs/heads/main/Competent/Competent.mp3"
+	}
+
+	m.Config = function(parent)
+		Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+	end
+
+	m.SaveConfig = function() return {} end
+	m.LoadConfig  = function(save) end
+
+	local animator = nil
+	local start    = 0
+
+	m.Init = function(figure)
+		SetOverrideDanceMusic(AssetGetContentId("Competent.mp3"), "Competent", 0.8, NumberRange.new(0, 999))
+
+		start           = os.clock()
+		animator        = AnimLib.Animator.new()
+		animator.rig    = figure
+		animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("Competent.anim"))
+		animator.looped = true
+		animator.speed  = 1
+	end
+
+	m.Update = function(dt, figure)
+		animator:Step(os.clock() - start)
+	end
+
+	m.Destroy = function(figure)
+		animator = nil
+	end
+
+	return m
+end)
+
 return modules
